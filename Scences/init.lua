8f2fa0def0afb5087beac504a289c4d624f270aa
Rmtdi <rmtdiyl@outlook.com>
2024-08-31 21:24:52 +0800

    更新了地图和地图材质

--- Scences/init.lua
+++ Scences/init.lua
@@ -2,12 +2,13 @@ Object = require"classic"
 require "snake"
 
 function love.load()
-    width = 32 * 27
-    height = 32 * 27
-    title = "SNAKY!!!"
 
     map_width = 25
     map_height = 25
+
+    width = 32 * 31
+    height = 32 * 27
+    title = "SNAKY!!!"
 
     love.window.setMode(width, height)
     love.window.setTitle(title)
